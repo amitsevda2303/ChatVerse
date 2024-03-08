2053ccb08a5f43e1ef34b23f08a754bdf4e046f0
amitsevda2303 <rohitkmt88@gmail.com>
2024-03-08 21:09:47 +0530

    make changes

--- client/src/Utils/ApiRoutes.js
+++ client/src/Utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000"
+export const host = "https://chatverse-603u.onrender.com"
 
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
