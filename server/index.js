c6a37b65b6604b27f8adc59879984ed2a8fc8328
amitsevda2303 <rohitkmt88@gmail.com>
2024-03-09 11:20:26 +0530

    socket origin change

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const server =  app.listen(process.env.PORT || 3000, () => {
 
 const io = socket(server,{
     cors:{
-        origin: "http://localhost:3000",
+        origin: "https://chattverse.netlify.app",
         credentials: true
     }
 })
